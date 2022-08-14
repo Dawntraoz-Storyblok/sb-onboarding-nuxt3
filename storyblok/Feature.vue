2c396ba1bb6c916c876bf9414f90db16421bcbf4
Dawntraoz <albasilventefu@gmail.com>
2022-08-14 13:45:50 +0200

    feat: step 4 - ultimate tutorial - new and updated components

--- storyblok/Feature.vue
+++ storyblok/Feature.vue
@@ -1,8 +1,11 @@
 <template>
-  <div v-editable="blok" class="w-full p-12 bg-[#f7f6fd] rounded-[5px] text-center">
-    <h3 class="text-2xl text-[#1d243d] font-bold">
-      {{ blok.name }}
-    </h3>
+  <div v-editable="blok" class="w-full bg-[#f7f6fd] rounded-[5px] text-center overflow-hidden">
+    <img :src="`${blok.image.filename}/m/`" :alt="blok.image.alt" class="w-full h-48 xl:h-72 object-cover" />
+    <div class="px-12 py-6">
+      <h3 class="text-2xl text-[#1d243d] font-bold">
+        {{ blok.name }}
+      </h3>
+    </div>
   </div>
 </template>
  
