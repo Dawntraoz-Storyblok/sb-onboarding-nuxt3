e3dec9f50e29ecf9f0091992f09f8b3578bb5bfd
Dawntraoz <albasilventefu@gmail.com>
2022-08-14 12:41:35 +0200

    feat: step 3 - ultimate tutorial - dynamic menu

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,7 +1,7 @@
 <script setup>
 const { slug } = useRoute().params
 
-const story = await useStoryblok(slug ? slug : 'home', {
+const story = await useStoryblok(slug ? slug.join('/') : 'home', {
   version: 'draft',
 })
 </script>
